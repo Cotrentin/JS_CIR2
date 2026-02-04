5bc7a24a0d08ee518995abe3e1a8793d7059e1a3
Coco <corentindubernet1@gmail.com>
2026-02-04 11:39:25 +0100

    TP_5

--- 2_les_variables/monscript.js
+++ 2_les_variables/monscript.js
@@ -2,3 +2,12 @@ let nom_jour = "Lundi";
 let numero_jour = 1;
 let nom_mois = "Janvier";
 let isHiver = true;
+let tabSemaine = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+let tabMois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
+let maDateV1 = {};
+maDateV1.jour = "Lundi";
+maDateV1.mois = "Janvier";
+let maDateV2 = {
+	"jour" : "Lundi",
+	"mois" : "Janvier"
+}
