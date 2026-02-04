b8fab7e27b57b086bbbddc452334f0dc3d4571ab
Coco <corentindubernet1@gmail.com>
2026-02-04 12:08:18 +0100

    TP_8

---
+++ 3_les_conditions/monscript.js
@@ -0,0 +1,10 @@
+let number = 5;
+let text = '5';
+let isRainingToday = true;
+
+console.log(number == text);       
+console.log(number === text);        
+
+if (isRainingToday === true) {
+    console.log("where is my umbrella?");
+}
