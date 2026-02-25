1b835280d7017bd768df7860663865e3d75150e1
Coco <corentindubernet1@gmail.com>
2026-02-25 11:30:09 +0100

    TP_13

---
+++ 6_DOM/monscript.js
@@ -0,0 +1,13 @@
+/* Toutes les variables */
+let li = document.querySelector("#listeCourses li");
+
+
+/* Tous les évènements */
+li.addEventListener('click', ()=>{
+    li.style.textDecoration = "line-through";
+    li.style.backgroundColor = "lightblue";
+})
+
+
+/* Les fonctions */
+// A voir plus tard
