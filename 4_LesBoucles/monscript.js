b2c240ccc5f00707a5a0c76bbe48054a05a3a5cb
Coco <corentindubernet1@gmail.com>
2026-02-25 09:39:17 +0100

    TP_12

--- 4_LesBoucles/monscript.js
+++ 4_LesBoucles/monscript.js
@@ -1,22 +1,40 @@
-// let i = 0;
-let somme = 0;
+//  let i = 0;
+// let somme = 0;
 
-// while (i < 101) {
-//     somme += i;
-//     i++;
+//  while (i < 101) {
+//      somme += i;
+//      i++;
 // }
 
 // console.log(somme);
-let j = 0;
-let i = prompt("choisissez un nombre :");
-if (isNaN(i)) {
-    alert("ce n'est pas un nombre, recommencez");
-}
+// let j = 0;
+// let i = prompt("choisissez un nombre :");
+// if (isNaN(i)) {
+//     alert("ce n'est pas un nombre, recommencez");
+// }
 
-else{
-    do {
-        somme += j;
-        j++;
- } while (j <= i);
- console.log(somme);
+// else{
+//     do {
+//         somme += j;
+//         j++;
+//  } while (j <= i);
+//  console.log(somme);
+// }
+
+// let tab = [];
+// if (tab.length < 10) {
+    // while (tab.length <= 8) {
+    //     tab.push(tab.length);
+    //     console.log(tab[tab.length - 1]);
+    // }
+    
+// }
+
+let tab = [];
+
+if (tab.length <= 10) {
+    for (let i = 0; i <= 10; i++) {
+        tab.push(i);
+        console.log(tab[tab.length - 1]);
+    }
 }
