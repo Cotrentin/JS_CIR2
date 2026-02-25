55572760d6c2c9facc1343ac859054f71b944613
Coco <corentindubernet1@gmail.com>
2026-02-25 12:18:33 +0100

    TP_16

--- 6_DOM/monscript.js
+++ 6_DOM/monscript.js
@@ -3,13 +3,41 @@ let li = document.querySelector("#listeCourses li");
 let dateJour = new Date(Date.now());
 let dateJourFr = dateJour.toLocaleDateString('fr-FR')
 let h2 = document.querySelector("h2");
+let Myimput = document.querySelector("#myInput");
+let Btn = document.querySelector(".btn");
+let ul = document.querySelector("#listeCourses");
 
-h2.textContent = "Liste de course " + dateJourFr;
+h2.textContent = "Liste de course :" + dateJourFr;
+
 li.addEventListener('click', ()=>{
     li.classList.toggle("itemCheck");
-    
 })
 
+Btn.addEventListener('click', ()=>{
+    addProduct();
+})
 
-/* Les fonctions */
-// A voir plus tard
+Myimput.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        addProduct();
+    }
+});
+
+function addProduct(){
+   let value = Myimput.value;
+
+   if(!value){
+    alert("Veuillez entrer un produit");
+    return;
+   }
+   else{
+    let li = document.createElement("li");
+    li.addEventListener('click', ()=>{
+    li.classList.toggle("itemCheck");
+    })
+    li.textContent = value;
+    ul.appendChild(li);
+    Myimput.value = "";
+    alert('produit ajouté');
+   }
+}
